9ed441b9363893146bb8687a80aea020e2462bb5
HKBUSeanTYH <seantyh118@gmail.com>
2021-10-06 00:47:32 +0800

    ktx kotlin room? coroutinescope unusable on windows??

--- app/src/main/java/comp4097/comp/hkbu/edu/hk/couponredemption/ui/coupons/CouponsListFragment.kt
+++ app/src/main/java/comp4097/comp/hkbu/edu/hk/couponredemption/ui/coupons/CouponsListFragment.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.room.CoroutinesRoom
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -34,84 +35,60 @@ class CouponsListFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_coupons_list, container, false)
-
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-//                adapter = CouponsRecyclerViewAdapter(PlaceholderContent.ITEMS)
-//                val couponImage = resources.getStringArray(R.array.couponImage)
-//                val couponRestaurant = resources.getStringArray(R.array.couponRestaurant)
-//                val couponDescription = resources.getStringArray(R.array.couponDescription)
-//                val couponCoins = resources.getStringArray(R.array.couponCoins)
-//
-//                val coupons =   mutableListOf<Coupons>()
-//                for (i in 0..(couponRestaurant.size - 1)){
-//                    coupons.add(Coupons(0,0,i,"",couponRestaurant[i],"",
-//                        "", couponImage[i],0, Integer.parseInt(couponCoins[i]),"",couponDescription[i]))
-//                }
-//
-//                adapter = CouponsRecyclerViewAdapter(coupons)
-                CoroutineScope(Dispatchers.IO).launch {
-                    val dao = AppDatabase.getInstance(context).couponsDao()
-                    val coupons = dao.getAllCoupons()
-                    CoroutineScope(Dispatchers.Main).launch {
-                        adapter = CouponsRecyclerViewAdapter(coupons)
-                    }
-                }
-            }
-        }
-        return view
-    }
-
 //    override fun onCreateView(
 //        inflater: LayoutInflater, container: ViewGroup?,
 //        savedInstanceState: Bundle?
 //    ): View? {
-//        val recyclerView = inflater.inflate(
-//            R.layout.fragment_coupons_list, container, false
-//        ) as RecyclerView
-//        recyclerView.layoutManager = LinearLayoutManager(context)
-//        reloadData(recyclerView)
-//        return recyclerView
-//    }
-//    private fun reloadData(recyclerView: RecyclerView) {
-//        val NEWS_URL = "https://5b07-158-182-198-90.ngrok.io/shop/json"
-//        CoroutineScope(Dispatchers.IO).launch {
-//            try {
-//                val json = Network.getTextFromNetwork(NEWS_URL)
-//                Log.d("JSON", json)
-//                val coupons =
-//                    Gson().fromJson<List<Coupons>>(json,object : TypeToken<List<Coupons>>() {}.type)
-//                Log.d("Read", coupons.toString())
-//                CoroutineScope(Dispatchers.Main).launch {
-//                    recyclerView.adapter = CouponsRecyclerViewAdapter(coupons)
-//                }
-//            } catch (e: Exception) {
-//                Log.d("CouponsListFragment", "Error in loading data")
-//                e.message?.let { Log.d("Error", it) }
-//                val coupons = listOf(
-//                    Coupons(
-//                        0,0,0, "Cannot fetch coupons",
-//                        "Please check your network connection,", "","","",0,
-//                        0,"",""
-//                    )
-//                )
+//        val view = inflater.inflate(R.layout.fragment_coupons_list, container, false)
 //
-//                CoroutineScope(Dispatchers.Main).launch {
-//                    recyclerView.adapter = CouponsRecyclerViewAdapter(coupons)
+//        // Set the adapter
+//        if (view is RecyclerView) {
+//            with(view) {
+//                layoutManager = when {
+//                    columnCount <= 1 -> LinearLayoutManager(context)
+//                    else -> GridLayoutManager(context, columnCount)
 //                }
+////                adapter = CouponsRecyclerViewAdapter(PlaceholderContent.ITEMS)
+////                val couponImage = resources.getStringArray(R.array.couponImage)
+////                val couponRestaurant = resources.getStringArray(R.array.couponRestaurant)
+////                val couponDescription = resources.getStringArray(R.array.couponDescription)
+////                val couponCoins = resources.getStringArray(R.array.couponCoins)
+////
+////                val coupons =   mutableListOf<Coupons>()
+////                for (i in 0..(couponRestaurant.size - 1)){
+////                    coupons.add(Coupons(0,0,i,"",couponRestaurant[i],"",
+////                        "", couponImage[i],0, Integer.parseInt(couponCoins[i]),"",couponDescription[i]))
+////                }
+////
+////                adapter = CouponsRecyclerViewAdapter(coupons)
 //            }
 //        }
+//        return view
 //    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val recyclerView = inflater.inflate(
+            R.layout.fragment_coupons_list, container, false
+        ) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        reloadData(recyclerView)
+        return recyclerView
+    }
+    private fun reloadData(recyclerView: RecyclerView) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val dao = AppDatabase.getInstance(context).couponsDao()
+
+            val coupons = dao.getAllCoupons()
+            CoroutineScope(Dispatchers.Main).launch {
+                adapter = CouponsRecyclerViewAdapter(coupons)
+            }
+        }
+
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 
     companion object {
 
